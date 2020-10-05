65ab1786e7728703c33a822690fb12bc9563e1ba
Valentina Rosales <valentina.rosales@correounivalle.edu.co>
2020-10-04 19:49:40 -0500

    Configuraciones de express

--- src/server.js
+++ src/server.js
@@ -3,7 +3,11 @@ const path = require('path');
 
 const app = express(); 
 
-app.use(express.static(__dirname+'/dist/automatizacion-front'));
+app.use(express.static(__dirname+'/../dist/automatizacion-front'));
 app.get('/', (req, res)  => {
-  res.sendFile(path.join(__dirname + '/dist/automatizacion-front/index.html'));
+  res.sendFile(path.join(__dirname + '/../dist/automatizacion-front/index.html'));
+})
+const port = process.env.port || 3000;
+app.listen(port, ()=> {
+  console.log(`Escuchando en el puerto: ${port}`);
 })
