ad13a5c8cf29788a3d87d36f845a4b0600274c25
Valentina Rosales <valentina.rosales@correounivalle.edu.co>
2020-10-04 19:09:32 -0500

    Funcionamiento en heroku

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express(); 
 
-app.use(express.static(__dirname+'/dist/'));
+app.use(express.static(__dirname+'/dist/automatizacion-front'));
 app.get('/', (req, res)  => {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/automatizacion-front/index.html'));
 })
