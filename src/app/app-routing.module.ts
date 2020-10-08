ce757bfc2b8e351ea96fc46a4864fa3cbf4efa81
Valentina Rosales <valentina.rosales@correounivalle.edu.co>
2020-10-08 10:09:36 -0500

    Mejoras en el renderizado de las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ControlComponent } from './components/control/control.component';
 import { ErpComponent } from './components/erp/erp.component';
+import { MateriaPrimaComponent } from './components/materia-prima/materia-prima.component';
 import { MesComponent } from './components/mes/mes.component';
+import { OrdenProduccionComponent } from './components/orden-produccion/orden-produccion.component';
 
 const routes: Routes = [
-  { path:'', component: ErpComponent},
+  { path:'', component: ErpComponent },
+  { 
+    path:'erp',
+    component: ErpComponent,
+    children: [
+      {
+        path: 'materia-prima',
+        component: MateriaPrimaComponent
+      },
+      {
+        path: 'orden-produccion',
+        component: OrdenProduccionComponent
+      },
+    ]
+  },
   { path:'mes', component: MesComponent},
   { path:'control', component: ControlComponent},
   { path: '**',  pathMatch: 'full', redirectTo:''}
