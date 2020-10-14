adc2f37c99d3e6003ec09aa2a8331c7c971640e8
Valentina Rosales <valentina.rosales@correounivalle.edu.co>
2020-10-14 16:43:19 -0500

    Mejora para las rutas

--- src/server.js
+++ src/server.js
@@ -4,9 +4,12 @@ const path = require('path');
 const app = express(); 
 
 app.use(express.static(__dirname+'/../dist/automatizacion-front'));
-app.get('/', (req, res)  => {
+app.get('*', (req, res)  => {
   res.sendFile(path.join(__dirname + '/../dist/automatizacion-front/index.html'));
 })
+
+
+
 const port = process.env.PORT || 3010;
 app.listen(port, ()=> {
   console.log(`Escuchando en el puerto: ${port}`);
