d02a2b5b59e1ed3108ca328f71ee58cb7be3f593
Valentina Rosales <valentina.rosales@correounivalle.edu.co>
2020-11-04 22:11:24 -0500

    Mejora desde el backend para la inicializacion de valores

--- src/app/services/orden-pedido.service.ts
+++ src/app/services/orden-pedido.service.ts
@@ -7,38 +7,7 @@ import { types } from '../types/types';
   providedIn: 'root',
 })
 export class OrdenPedidoService {
-  constructor(private api: ApiService) { 
-    this.inicializarValores();
-  }
-
-  inicializarValores() {
-    this.consultarData(types.API.ReferenciaProductos).then((res: any) => {
-      if (!res || res.data.length == 0) {
-        this.guardarData(types.API.ReferenciaProductos,{descripcion: 'Shampoo'});
-        this.guardarData(types.API.ReferenciaProductos,{descripcion: 'Acondicionador'})
-      }
-    })
-    this.consultarData(types.API.TipoProductos).then((res: any) => {
-      if (!res || res.data.length == 0) {
-        this.guardarData(types.API.TipoProductos,{descripcion: 'Lisos'});
-        this.guardarData(types.API.TipoProductos,{descripcion: 'Risos'});
-        this.guardarData(types.API.TipoProductos,{descripcion: 'Duos'});
-      }
-    })
-    this.consultarData(types.API.Prioridades).then((res: any) => {
-      if (!res || res.data.length == 0) {
-        this.guardarData(types.API.Prioridades, {descripcion: 'Alta', nivel:1});
-        this.guardarData(types.API.Prioridades, { descripcion: 'Media', nivel: 2});
-        this.guardarData(types.API.Prioridades, { descripcion: 'Baja', nivel: 3});
-      }
-    })
-    this.consultarData(types.API.PresentacionProductos).then((res: any) => {
-      if (!res || res.data.length == 0) {
-        this.guardarData(types.API.PresentacionProductos,{descripcion: '450 ml', cantidad:450});
-        this.guardarData(types.API.PresentacionProductos,{descripcion: '250 ml', cantidad: 250});
-      }
-    })
-  }
+  constructor(private api: ApiService) { }
 
   consultarReferenciasProducto(){
     return this.consultarData(types.API.ReferenciaProductos);
